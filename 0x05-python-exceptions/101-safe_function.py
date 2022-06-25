8cac62b4c6f7afbe1e8011ea07c1917584a39084
29trix <gesystemz1@gmail.com>
2022-06-25 17:26:49 +0100

    bare except fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,10 +3,10 @@
 import sys
 
 
-def safe_function(fct, *args):
+def safe_function(a, *args):
     try:
-        result = fct(*args)
+        result = a(*args)
         return (result)
-    except:
+    except Exception as a:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
